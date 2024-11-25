d62c66cdaaf3019429e29f5431c1674804224ccc
flask599 <flask599@gmail.com>
2024-11-25 20:00:00 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,35 @@
+print('1-ое решение')
+name = 'Danil'
+age = 32
+is_student = True or False
+print('Name: ', name)
+print('Age: ', age)
+print('New age: ', age + 5)
+print('Is student: ',is_student)
+
+print('')
+print('2-ое решение')
+name = 'Danil'
+age = 32
+new_age =  5
+Yes = 'True'
+No = 'False'
+is_student = Yes or No
+print('Name: ', name)
+print('Age: ', age)
+print('New age: ', age + new_age)
+print('Is student: ', is_student)
+
+print('')
+print('3-ое решение')
+name = 'Danil'
+age = int('32')
+new_age =  age + int('-5')
+Yes = True
+No = False
+is_student = Yes==No or Yes!=No and No==No
+print('Name: ', name)
+print('Age: ', age)
+print('New age: ', new_age)
+print('Is student: ',is_student)
+
